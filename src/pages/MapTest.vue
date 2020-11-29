8071ae4962ae7a98ae1565d03793819c052ced5e
alerokhi <Aleksandr.Erokhin@t-systems.com>
2020-11-29 17:11:50 +0300

    Add autocomplete component for geocoder

--- src/pages/MapTest.vue
+++ src/pages/MapTest.vue
@@ -1,31 +1,36 @@
 <template>
     <div>
-        <b-form>
-            <b-form-input v-model="searchVal"></b-form-input>
-            <b-button @click="search()">Поиск</b-button>
-        </b-form>
-        {{ res }}
+        <autocomplete :search="search"
+        placeholder="Введите адрес"></autocomplete>
     </div>
 </template>
 
 <script>
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+import '@trevoreyre/autocomplete-vue/dist/style.css'
+
 export default {
+    components: {
+        Autocomplete,
+    },
     data: function () {
         return {
             ymaps: null,
             searchVal: "",
-            res: [],
+            res: []
         };
     },
     methods: {
-        search() {
-            window.ymaps.geocode(this.searchVal).then((result) => {
-                this.res =
-                    result.geoObjects._collectionComponent._baseArrayComponent._children
-                    .map(item => {
-                      return item._geoObjectComponent._properties._data.text
-                    });
-                console.log(this.res);
+        search(input) {
+            if (input.length < 3) {
+                return [];
+            }
+            return window.ymaps.geocode("Санкт-Петербург " + input).then((result) => {
+                return result.geoObjects._collectionComponent._baseArrayComponent._children.map(
+                    (item) => {
+                        return item._geoObjectComponent._properties._data.text;
+                    }
+                );
             });
         },
     },
@@ -36,16 +41,10 @@ export default {
             .then(() => {
                 window.ymaps.ready(() => {
                     this.ymaps = window.ymaps;
-                    // window.ymaps.geocode("Народного ополчения").then((res) => {
-                    //     this.res =
-                    //         res.geoObjects._collectionComponent._baseArrayComponent._children;
-                    //     console.log(this.res);
-                    // });
                 });
             })
             .catch((e) => {
                 console.log("failed to load map" + e);
-                // Failed to fetch script
             });
     },
 };
