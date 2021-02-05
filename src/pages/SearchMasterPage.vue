00183377a02106b4a1eff0d7d2e6cfffd83f7108
alerokhi <Aleksandr.Erokhin@t-systems.com>
2021-02-05 19:17:29 +0300

    Work with use geoposition

--- src/pages/SearchMasterPage.vue
+++ src/pages/SearchMasterPage.vue
@@ -4,6 +4,8 @@
         <b-row id="main">
             <b-col md="3" class="d-md-block d-none navigation">
             </b-col>
+            {{lat}}
+                {{long}}
             <MobileTopBar />
             <MasterSearchResults />
         </b-row>
@@ -12,17 +14,36 @@
 </template>
 
 <script>
-import MobileTopBar from '../components/MobileTopBar.vue';
-import MasterSearchResults from '../components/MasterSearchResults.vue';
-import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue'
+import MobileTopBar from "../components/MobileTopBar.vue";
+import MasterSearchResults from "../components/MasterSearchResults.vue";
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
+    data: function() {
+        return {
+            lat: null,
+            long: null
+        }
+    },
     components: {
         MobileTopBar,
         MasterSearchResults,
         Header,
-        Footer
+        Footer,
+    },
+    mounted() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.lat = position.coords.latitude;
+                this.long = position.coords.longitude;
+                console.log(position.coords.latitude);
+                console.log(position.coords.longitude);
+            },
+            (error) => {
+                console.log(error.message);
+            }
+        );
     },
 };
 </script>
